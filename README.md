e81232d2e3d5f805c148f6e6536f46d36233acca
Jon Ross <zlsa@outlook.com>
2014-09-26 12:49:16 -0700

    second commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Orbital Pinball!
+
+Inspired by
+[this](http://www.reddit.com/r/KerbalSpaceProgram/comments/2hhxzz/my_buddy_is_an_electrical_engineer_he_actually/ckt0ypq)
+reddit thread.
+
+# License
+
+CC0.
---
+++ assets/scripts/canvas.js
@@ -0,0 +1,48 @@
+
+function canvas_init_pre() {
+  prop.canvas={};
+
+  prop.canvas.contexts={};
+
+  // resize canvas to fit window?
+  prop.canvas.resize=false;
+  prop.canvas.size={ // all canvases are the same size
+    height:640,
+    width:480
+  };
+}
+
+function canvas_init() {
+//  canvas_add("overlay");
+}
+
+function canvas_resize() {
+  if(prop.canvas.resize) {
+    prop.canvas.size.width=$(window).width();
+    prop.canvas.size.height=$(window).height();
+  }
+  for(var i in prop.canvas.contexts) {
+    prop.canvas.contexts[i].canvas.height=prop.canvas.size.height;
+    prop.canvas.contexts[i].canvas.width=prop.canvas.size.width;
+  }
+}
+
+function canvas_add(name) {
+  $("#canvases").append("<canvas id='"+name+"-canvas'></canvas>");
+  prop.canvas.contexts[name]=$("#"+name+"-canvas").get(0).getContext("2d");
+}
+
+function canvas_get(name) {
+  return(prop.canvas.contexts[name]);
+}
+
+function canvas_clear(cc) {
+  cc.clearRect(0,0,prop.canvas.size.width,prop.canvas.size.height);
+}
+
+function canvas_update() {
+  // var cc=canvas_get("overlay");
+  // cc.save();
+  // canvas_clear(cc);
+  // cc.restore();
+}
